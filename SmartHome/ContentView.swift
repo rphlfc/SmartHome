ac14087cbc8ab58501bd0cb8f4f98d5d2d64e096
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-09-12 20:15:17 -0300

    Initial Commit

---
+++ SmartHome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartHome
+//
+//  Created by Raphael Cerqueira on 12/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartHome/SmartHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Raphael Cerqueira on 12/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
